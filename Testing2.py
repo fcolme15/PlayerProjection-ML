6fe8a321f18b3c5171dfca6d04fe8b164e1f2737
Francisco Colmenero <franciscojcolmenero@outlook.com>
2024-11-14 18:15:56 -0600

    Initial push

--- Testing2.py
+++ Testing2.py
@@ -1,5 +1,35 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns #Plotting library #pip install seaborn
+
 import missingno as msno #Library to drop missing data #pip install missingno 
-#import scikit
+
+#sklearn ->pip install scikit-learn
+from sklearn.linear_model import LogisticRegression 
 
 
+def main():
+
+    #Model 1, Logistic Regression:
+    # Initialize a logistic regression model
+    logisticModel = LogisticRegression(penalty=None)
+
+    # Fit the model
+    logisticModel.fit(X, np.ravel(y))
+
+    # Print the fitted model
+    print('w1:', logisticModel.coef_)
+    print('w0:', logisticModel.intercept_)
+
+
+    #Model 2, K-means Regression:
+
+
+    #Model 2, K-means Regression:
+
+
+
+
+if __name__ == "__main__":
+    main()
